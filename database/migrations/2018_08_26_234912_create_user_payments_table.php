a7f04e3c2ef0e003a919cd75263555fd4497cf57
juba <abdo838@gmail.com>
2018-08-27 03:06:10 +0200

    add migrations

---
+++ database/migrations/2018_08_26_234912_create_user_payments_table.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateUserPaymentsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('user_payments', function (Blueprint $table) {
+            $table->increments('id')->unsigned();
+            $table->unsignedInteger('user_id');
+            $table->enum('type',['debit','credit']);
+            $table->integer('balance_after');
+            $table->integer('balance_before');
+            $table->timestamps();
+            $table->foreign('user_id')
+                ->references('id')
+                ->on('users')
+                ->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('user_payments', function($table){
+            $table->dropForeignIfExists(['user_id']);
+        });
+    }
+}
